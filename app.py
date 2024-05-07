ccd9db170fe5bc9b94b8474867635c120ae86917
OumaArera <johnouma999@gmail.com>
2024-05-07 21:20:50 +0300

    feat: initialize db, migrate, and upgrade the db

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, make_response, jsonify
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from sqlalchemy.exc import IntegrityError
+from models import db
+from datetime import datetime, timezone, timedelta
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+from models import User,  RoutePlan, Location, Outlet, Notification, ActivityLog
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://mroutedb_user:XaoUcFYT9JBGqazs9XIMgvhMK4JnC214@dpg-cot6jg4f7o1s73e1rkag-a.oregon-postgres.render.com/mroutedb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
+# app.config["JWT_SECRET_KEY"] = "merchandiser route tracker"
+
+
+db.init_app(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
+bcrypt = Bcrypt(app)
+
+api = Api(app)
+CORS(app)
+
+
+
+
+
